3d8488a405c7c1f4124caa2bc8e4252f325b7d17
jthpotz <jthpotz@gmail.com>
2017-11-10 16:01:04 -0800

    set up main scene

--- Assets/Scripts/GlobalConstants.cs
+++ Assets/Scripts/GlobalConstants.cs
@@ -8,6 +8,6 @@ public class GlobalConstants
 	public static float MapHeight = 1.5f;
 	public static float PlayerSpeed = 0.2f;
 	public static float PlayerStoppingDist = 0.01f;
-
+	public static int GameLength = 5; // Minutes
 }
 
