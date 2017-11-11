8d2486cb4420d61968489a9be79a2ffb0c7852f3
Alex Gravenor <blazingkin@gmail.com>
2017-11-10 16:34:30 -0800

    Update main level

--- Assets/Scripts/GlobalConstants.cs
+++ Assets/Scripts/GlobalConstants.cs
@@ -6,10 +6,10 @@ public class GlobalConstants
 
 	public static float MapWidth = 16f;
 	public static float MapHeight = 9f;
-	public static float PlayerSpeed = 0.2f;
-	public static float PlayerStoppingDist = 0.01f;
-	public static float PlayerInteractionDistance = 0.25f;
-	public static float InteractionDistance = 0.25f;
+	public static float PlayerSpeed = 1f;
+	public static float PlayerStoppingDist = 0.2f;
+	public static float PlayerInteractionDistance = .5f;
+	public static float InteractionDistance = 0.5f;
 	public static int GameLength = 5; // Minutes
 
 }
