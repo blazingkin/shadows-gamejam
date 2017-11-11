baee7d9a8c2086a577fcbc4c60b01a6e992fe193
jthpotz <jthpotz@gmail.com>
2017-11-10 16:25:45 -0800

    world clock and camera size

--- Assets/Scripts/GlobalConstants.cs
+++ Assets/Scripts/GlobalConstants.cs
@@ -4,11 +4,11 @@ using System.Collections;
 public class GlobalConstants
 {
 
-	public static float MapWidth = 1.8f;
-	public static float MapHeight = 1.5f;
+	public static float MapWidth = 16f;
+	public static float MapHeight = 9f;
 	public static float PlayerSpeed = 0.2f;
 	public static float PlayerStoppingDist = 0.01f;
-	public static int GameLength = 5; // Minutes
+	public static float GameLength = .5f; // Minutes
 	public static float PlayerInteractionDist = 0.1f;
 
 }
