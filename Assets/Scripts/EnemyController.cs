63bb57df75979100cbf6012b602916844ccd8e18
Alex Gravenor <blazingkin@gmail.com>
2017-11-10 18:42:14 -0800

    Enemy movement

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -6,13 +6,28 @@ public class EnemyController : MonoBehaviour {
 
 	public static List<GameObject> enemies = new List<GameObject> ();
 
+	private Rigidbody2D rbody;
+	private GameObject enemy;
+	public float timePassed = 0f;
+	public float timeToCheck = 0.75f;
 	// Use this for initialization
 	void Start () {
-		
+		enemy = gameObject;
+		rbody = GetComponent<Rigidbody2D> ();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void FixedUpdate () {
+		rbody.AddForce (new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f)));
+		timePassed += Time.deltaTime;
+		if (timePassed > timeToCheck) {
+			if (LightSource.InLight (enemy.transform.position)) {
+				enemy.GetComponent<SpriteRenderer> ().enabled = false;
+			} else {
+				enemy.GetComponent<SpriteRenderer> ().enabled = true;
+			}
+			timePassed = 0;
+		}
 	}
+
 }
