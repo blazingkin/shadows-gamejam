fec79f56c0686f1f9212aabda4328e3d5bac8abc
blazingkin <blazingkin@gmail.com>
2017-11-11 13:20:05 -0800

    Matches now spawn and can be picked up

---
+++ Assets/Scripts/Match.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Match : MonoBehaviour {
+
+	GameObject match;
+	// Use this for initialization
+	void Start () {
+		match = gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (PositionMath.playerInteractedWith (match.transform.position)) {
+			Destroy (match);
+			PlayerData.Matches++;
+		}
+	}
+}
