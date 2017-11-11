dddfb62f359312acb594287f1eb5ef793d8288f4
jthpotz <jthpotz@gmail.com>
2017-11-10 16:38:49 -0800

    world clock handles end of game actualy

--- Assets/Scripts/WordClock.cs
+++ Assets/Scripts/WordClock.cs
@@ -14,11 +14,16 @@ public class WordClock : MonoBehaviour {
 		moon = GameObject.Instantiate (Resources.Load ("Prefabs/Moon"), new Vector2(-15.5f, 8.5f), Quaternion.identity) as GameObject;
 		rbody = moon.GetComponent<Rigidbody2D> ();
 		speed = new Vector2 ((2*GlobalConstants.MapWidth)/(gameLength), 0);
-		Debug.Log (speed);
 		rbody.velocity = speed;
 	}
 
 	// Update is called once per frame
 	void Update () {
+		if(moon.transform.position.x >= 15.5){
+			Debug.Log ("Game Over");
+			GameObject nil = null;
+			nil.transform.position = transform.position;
+		}
 	}
+
 }
