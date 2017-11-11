1edb2ed4c60c3f56729ea0bd7269c8ec830b4a65
jthpotz <jthpotz@gmail.com>
2017-11-11 13:36:40 -0800

    flickering lights on main menu

---
+++ Assets/Scripts/FlickeringLight.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlickeringLight : MonoBehaviour {
+
+	Light light;
+	public float intensity;
+	public float minIntensity = 3f, maxIntesity = 5f;
+	public float timeDelay = .05f;
+	float time =  0;
+
+	// Use this for initialization
+	void Start () {
+		light = GetComponent<Light> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		time += Time.deltaTime;
+		while (time > timeDelay) {
+			time -= timeDelay;
+			intensity = Random.Range (minIntensity, maxIntesity);
+			light.intensity = intensity;
+		}
+	}
+}
