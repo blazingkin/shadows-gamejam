145d209482a7b045e5968dfa5062d71dd06e9e53
blazingkin <blazingkin@gmail.com>
2017-11-11 16:45:50 -0800

    Fix campfires

--- Assets/Scripts/GlobalConstants.cs
+++ Assets/Scripts/GlobalConstants.cs
@@ -21,12 +21,12 @@ public class GlobalConstants
 	public static float PlayerStoppingDist = 0.2f;
 
 
-	public static float PlayerInteractionDistance = 1f;
+	public static float PlayerInteractionDistance = 1.5f;
 
 	public static int PlayerStartingMatches = 3;
 	public static float PlayerStartingHealth = 3;
 
-	public static float InteractionDistance = 1f;
+	public static float InteractionDistance = 1.5f;
 
 
 	public static float EndMoonPos = 15.5f;
