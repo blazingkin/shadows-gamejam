3fb18d46f0fd182865f5dfdea70b0fced5b5bda0
blazingkin <blazingkin@gmail.com>
2017-11-11 16:19:56 -0800

    Explosive Torch

--- Assets/Scripts/FlammableObjects/ExplosiveTorch.cs
+++ Assets/Scripts/FlammableObjects/ExplosiveTorch.cs
@@ -6,15 +6,19 @@ public class ExplosiveTorch : FlammableObject
 
 	public override void onLit(){
 		base.onLit ();
-
+		Invoke ("onExtinguish", 10);
 	}
 
 	public override void onExtinguish(){
 		base.onExtinguish ();
+		GameObject.Instantiate (Resources.Load ("Prefabs/ParticleEffects/Explosion"), gameObject.transform.position, Quaternion.identity);
+		if ((PositionMath.getPlayerPosition() - gameObject.transform.position).magnitude < GlobalConstants.torchExplosionRadius){
+			PlayerData.Damage(0.5f);
+		}
 	}
 
 	public override float ExtinguishProbability(){
-		return 0.2f;
+		return 0f;
 	}
 
 
